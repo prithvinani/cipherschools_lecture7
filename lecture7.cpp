b2933d2b040c990c8839e845edbd7bdc7f4a67b8
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-06-27 02:50:11 +0530

    Add files via upload

---
+++ lecture7.cpp
@@ -0,0 +1,64 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int password;
+//     cout<<"enter the Password: ";
+//     do {
+//         cin>>password;
+//     }
+//     while(password<99999)
+//     // {
+//     //     cout<<"please enter the password again: ";
+//     //     cin>>password;
+//     // }
+//     // cout<<"the user has now entered a correct password"<<endl;
+
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int password;
+//     while(password<99){
+//         cout<<"chocolates"<<endl;
+//         cin>>password;
+//     }
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int password;
+//     int count;
+
+//     while(1){
+//         cout<<"chocolate";
+//         count++;
+
+//         if(count>100) break;
+//     }
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0; ; i++){
+        cout<<i<<" ";
+        if(i>100) 
+        break;
+    }
+    return 0;
+}
+
+
+
+
